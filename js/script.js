3aeac8bed01246bf54b6448ea94e1e4d8bb713d2
Stas <irshenko070@>
2023-03-07 20:14:09 +0200

    create repo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+    });
+  });
