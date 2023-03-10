73a9efee48850dde4f399caa82a49f20fc9680c8
Stas <irshenko070@>
2023-03-10 20:13:58 +0200

    minor upd script imagesn

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,        
         arrows: true,
-        prevArrow:'<button type="button" class="slick-prev"><img src="../img/prev.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/next.png"></button>',
+        prevArrow:'<button type="button" class="slick-prev"><img src="img/prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/next.png"></button>',
     });
 
     function toggleSlide(item){
