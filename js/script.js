79c03777be2767be2b0006e6cb5b38ee0ab5d7c0
Stas <irshenko070@>
2023-03-10 20:02:56 +0200

    complete abot us and donate btn

--- js/script.js
+++ js/script.js
@@ -4,15 +4,26 @@ $(document).ready(function(){
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1500,
+        arrows: false,
+    });
+    $('.about__cards').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1,        
         arrows: true,
+        prevArrow:'<button type="button" class="slick-prev"><img src="../img/prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/next.png"></button>',
     });
 
-
-    $('.about-item__link').each(function(i) {
-        $(this).on('click', function(e){
-            e.preventDefault();
-            $('.about-item__content').eq(i).toggleClass('about-item__content_active');
-            $('.about-item__list').eq(i).toggleClass('about-item__list_active');
-        })
-    })
+    function toggleSlide(item){
+        $(item).each(function(i) {
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.about-item__content').eq(i).toggleClass('about-item__content_active');
+                $('.about-item__list').eq(i).toggleClass('about-item__list_active');
+            })
+        });       
+    };
+    toggleSlide('.about-item__link');
+    toggleSlide('.about-item__list__link');
   });
