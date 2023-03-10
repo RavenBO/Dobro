4e64a520e2c8b480a6297f49685cf536fca1653c
Stas <irshenko070@>
2023-03-10 17:08:45 +0200

    Create and style about us

--- js/script.js
+++ js/script.js
@@ -4,11 +4,15 @@ $(document).ready(function(){
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1500,
-        arrows: false,
+        arrows: true,
     });
-    $('.about-slider').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        arrows: true
-    });
+
+
+    $('.about-item__link').each(function(i) {
+        $(this).on('click', function(e){
+            e.preventDefault();
+            $('.about-item__content').eq(i).toggleClass('about-item__content_active');
+            $('.about-item__list').eq(i).toggleClass('about-item__list_active');
+        })
+    })
   });
